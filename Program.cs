fb1de52fbbaec234aac7c9f8ef7e10f6fc066bea
Marcos-Santos <marcos_alexandre@msn.com>
2022-08-04 21:23:11 -0300

    Atualizando arquivos

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Meu primeiro programa em c# diferencas entre versoes .Net ler site abaixo
+// See https://aka.ms/new-console-template for more information
+
+/*
+#Program para dotNet6.0 ou superior (não necessario utilizar declarar Usings e criar a class Main
+o compilador realiza todas essas estapas de forma implicitas)
+*/
+
+
+/* Console.WriteLine("Hello, World!");
+Console.WriteLine("Meu primeiro programa em C# em .Net 6");
+Console.WriteLine("Marcos A L Santos 28/07/2022"); */
+
+
+
+//Program para dotNet 5.x ou inferior
+
+namespace MyHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Meu primeiro programa em C# em code .Net 5");
+            Console.WriteLine("Marcos A L Santos 28/07/2022");
+        }
+    }
+}
